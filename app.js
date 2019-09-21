dde6ff4de6ca0833256000c0119e0b7936b8eacc
MansiNegi27 <33727347+MansiNegi27@users.noreply.github.com>
2019-09-21 23:01:18 +0530

    Mp Working

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(express.static(path.join(__dirname,'client')));
 const client =require('./index.js');
 app.use('/',client);
 
-
 app.listen(4600,()=>{
   console.log(`listening on port 4600`);
 });
--- mood_predictor.py
+++ mood_predictor.py
@@ -7,7 +7,7 @@ dict={
     'Happy':['cheerful','contented','delighted','ecstatic','elated','glad','joyful','joyous','jubilant','lively','good','light'],
     'Sad':['not good','sad','low','depressed','bitter','dismal','heartbroken','melancholy','mournful','pessimistic','somber','sorrowful','sorry','wistful'],
     'Angry':['annoyed','bitter','enraged','exasperated','furious','heated','impassioned','indignant','irate','irritable','irritated']
-    
+
 }
 
 for emo,val in dict.items():    # for name, age in dictionary.iteritems():  (for Python 2.x)
