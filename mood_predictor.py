6884177f95a0b66dda3e2c141239a46b130398aa
Ambika <ambika.arora1998@gmail.com>
2019-09-21 21:23:56 +0530

    mood prediction added

--- mood_predictor.py
+++ mood_predictor.py
@@ -1,4 +1,18 @@
 import sys
-result="Bye";
-print(result);
-sys.stdout.flush();
+
+
+text=sys.argv[1]
+
+dict={
+    'Happy':['cheerful','contented','delighted','ecstatic','elated','glad','joyful','joyous','jubilant','lively','good','light'],
+    'Sad':['not good','sad','low','depressed','bitter','dismal','heartbroken','melancholy','mournful','pessimistic','somber','sorrowful','sorry','wistful'],
+    'Angry':['annoyed','bitter','enraged','exasperated','furious','heated','impassioned','indignant','irate','irritable','irritated']
+    
+}
+
+for emo,val in dict.items():    # for name, age in dictionary.iteritems():  (for Python 2.x)
+    for i in val:
+        if(i in text):
+            print(emo)
+            break
+sys.stdout.flush()
