e806337cf8f38eb884a1aadea5a90f19ab0d8b48
MansiNegi27 <33727347+MansiNegi27@users.noreply.github.com>
2019-09-22 05:43:59 +0530

    Recommendations added

--- client/index.js
+++ client/index.js
@@ -1,5 +1,76 @@
 const stopRecording = document.getElementById('stopRecording');
 stopRecording.addEventListener("click",stopRec);
+const startRecording = document.getElementById('startRecording');
+startRecording.addEventListener("click",startRec);
+
+function handleRecommendations()
+{
+  const tag = document.getElementById('chosenLabel').classList[0];
+  const http = new XMLHttpRequest();
+  const url = 'http://localhost:4600/recommendations';
+  http.open('POST',url,true);
+  http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  http.setRequestHeader('DATA', tag);
+  http.send();
+}
+
+function startRec()
+{
+  const startRecording = document.getElementById('startRecording');
+  const icon = document.createElement('i');
+  startRecording.innerHTML ="";
+  icon.classList.add('fa');
+  icon.classList.add('fa-microphone');
+  icon.classList.add('fa-2x');
+  startRecording.appendChild(icon);
+  const textbox = document.getElementById('text');
+  text.classList.remove('dontshow');
+  const stopRecord = document.getElementById('stopRecord');
+  stopRecord.classList.remove('dontshow');
+}
+
+function showEmojis(predictedLabel,pictureQuote)
+{
+  const heading = document.createElement('h3');
+  const icon = document.createElement('h1');
+  const chosenLabel = document.getElementById('chosenLabel');
+  chosenLabel.classList.add(predictedLabel)
+  const goRecommend = document.createElement('button');
+  goRecommend.classList.add('btn');
+  goRecommend.classList.add('btn-primary');
+  goRecommend.classList.add('recommend');
+  goRecommend.innerHTML = "See the suggestions";
+  heading.innerHTML = pictureQuote;
+  heading.classList.add('quote');
+  const app = document.getElementById('app');
+  app.appendChild(heading);
+  app.appendChild(icon);
+  app.appendChild(goRecommend);
+  const emos = {
+    "Angry" : "&#128545",
+    "Sad" : 	"&#128542",
+    "Happy" : "&#128516"
+  }
+  icon.innerHTML = emos[predictedLabel];
+  icon.classList.add('myEmoji');
+  goRecommend.addEventListener('click',handleRecommendations);
+}
+
+function showPrediction(predictedLabel)
+{
+    const app = document.getElementById('app');
+    app.innerHTML = "";
+    const quote = {
+      "Angry" :"Why so angry ? You need to calm down",
+      "Sad":"Please don't be sad :( , Smile",
+      "Happy":"Yayy, let's spread more happiness"
+    };
+    const pictureQuote = quote[predictedLabel];
+    if(pictureQuote.length>0)
+    {
+      showEmojis(predictedLabel,pictureQuote);
+    }
+}
 
 function stopRec()
 {
@@ -10,10 +81,22 @@ function stopRec()
   http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   http.setRequestHeader('DATA', text.value);
   http.onload = (e) =>{
-    console.log(http.responseText);
+      const predictedLabel =  (JSON.parse(http.response)).data;
+      const length = predictedLabel.length;
+      const finalLabel = predictedLabel.substr(0,length-2);
+      const expectedLabel = ["Angry","Sad","Happy"];
+      if(expectedLabel.indexOf(finalLabel) != -1)
+      {
+        showPrediction(finalLabel);
+      }else{
+        console.log("Wrong Label");
+        window.alert("Not recognition");
+      }
   };
   http.send();
 }
+
+
 var SpeechRecognition = window.webkitSpeechRecognition;
 
 var recognition = new SpeechRecognition();
--- mood_predictor.py
+++ mood_predictor.py
@@ -4,9 +4,9 @@ import sys
 text=sys.argv[1]
 
 dict={
-    'Happy':['cheerful','contented','delighted','ecstatic','elated','glad','joyful','joyous','jubilant','lively','good','light'],
+    'Happy':['happy','cheerful','contented','delighted','ecstatic','elated','glad','joyful','joyous','jubilant','lively','good','light'],
     'Sad':['not good','sad','low','depressed','bitter','dismal','heartbroken','melancholy','mournful','pessimistic','somber','sorrowful','sorry','wistful'],
-    'Angry':['annoyed','bitter','enraged','exasperated','furious','heated','impassioned','indignant','irate','irritable','irritated']
+    'Angry':['angry','annoyed','bitter','enraged','exasperated','furious','heated','impassioned','indignant','irate','irritable','irritated']
 
 }
 
